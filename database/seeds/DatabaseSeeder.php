b00aaee21f1f96d5820992a70fd3b2bfe379ea47
Olivier <oliverbolangala@gmail.com>
2022-06-03 23:00:27 +0200

    Seeding for products and reviews

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Model\Product;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,8 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        factory(App\Model\Product::class,50)->create();
+        factory(App\Model\Review::class,300)->create();
     }
 }

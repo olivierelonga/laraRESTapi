0d3c6698645f3f799a637220e14a9718898ed1aa
Olivier <oliverbolangala@gmail.com>
2022-06-04 07:48:54 +0200

    Product resource created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Model\Product;
+use App\Http\Resources\Product\ProductResource;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
     //
+    public function index(){
+    return Product::all();
+    }
+
+    public function edit(Product $product){
+        return Product::all();
+        }
+
+    public function show(Product $product){
+            // return $product;
+            return new ProductResource($product);
+        }
+
 }

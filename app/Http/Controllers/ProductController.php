11f94db802e4cd70f03ce6551e38ea47a50611a7
Olivier <oliverbolangala@gmail.com>
2022-06-06 16:09:04 +0200

    Product creation

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 use App\Model\Product;
 use App\Http\Resources\Product\ProductResource;
+use App\Http\Request\ProductRequest;
 use App\Http\Resources\Product\ProductCollection;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 class ProductController extends Controller
 {
+    public function __construct(){
+        $this->middleware('auth:api')->except('index','show');
+    }
     //
     public function index(){
 
@@ -23,6 +28,23 @@ class ProductController extends Controller
     public function show(Product $product){
             // return $product;
             return new ProductResource($product);
+        }
+
+
+        public function store(ProductRequest $request){
+
+            // return $productRequest::all();
+            $product = new Product;
+            $product->name = $request->name;
+            $product->name = $request->description;
+            $product->name = $request->stock;
+            $product->name = $request->price;
+            $product->name = $request->discount;
+            $product->save();
+
+            return response([
+                'data' => new ProductRequest($request)
+            ],201);
         }
 
 }
